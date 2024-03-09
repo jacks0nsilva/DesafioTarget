c1be6d70781ef022ab838757f5982ad7947e62d9
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2024-03-09 13:28:50 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioTarget/PhaseReverse.cs
@@ -0,0 +1,25 @@
+﻿namespace DesafioTarget;
+
+public static class PhaseReverse
+{
+    public static string Reverse(string phrase)
+    {
+        // converte a string em um array de caracteres
+        char[] caracteres = phrase.ToCharArray();
+
+        for (int i = 0; i < caracteres.Length / 2; i++)
+        {
+           // variavel temporaria recebe o caractere na posição i
+            char temporario = caracteres[i];
+
+            // o caractere na posição i recebe o caractere na posição caracteres.Length - i - 1
+            caracteres[i] = caracteres[caracteres.Length - i - 1];
+
+            // o caractere na posição caracteres.Length - i - 1 recebe o caractere na posição i
+            caracteres[caracteres.Length - i - 1] = temporario;
+        }
+
+        // converte o array de caracteres de volta para uma string
+        return new string(caracteres);
+    }
+}
---
+++ DesafioTarget/Program.cs
@@ -0,0 +1,26 @@
+﻿using DesafioTarget;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.ForegroundColor = ConsoleColor.Yellow;
+bool executarNovamente = true;
+
+while (executarNovamente)
+{
+    Console.WriteLine("\nOlá! Seja bem-vindo ao programa phrase reverse, conseguimos inverter qualquer frase que você deseja");
+    Console.Write("\nDigite a frase que deseja inverter: ");
+
+    string dadosDeEntrada = Console.ReadLine();
+    string dadosDeSaida = PhaseReverse.Reverse(dadosDeEntrada);
+
+
+    Console.WriteLine($"\nSua frase invertida é: {dadosDeSaida}");
+
+    Console.Write("Deseja executar novamente? (S/N): ");
+    char resposta = char.ToUpper(Console.ReadKey().KeyChar);
+    Console.WriteLine();
+    executarNovamente = (resposta == 'S');
+}
+
+Console.WriteLine("Obrigado por utilizar o programa phrase reverse! Volte sempre! :)");
+
